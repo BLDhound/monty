b3bd5d6c0ba61b3e14a57b4238c62300a54d3748
Lekarri <lekarrikaila567@gmail.com>
2022-12-16 06:50:48 -0800

    monty interpreter

---
+++ op_nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * op_nop - does nothing
+ * @stack: double pointer to head node
+ * @line_number: line being interpreted
+ *
+ * Return: nothing
+ */
+void op_nop(stack_t **stack, unisgned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
